5cb8365e50e757aa09be5d7628fbef485864c762
Timothy Rodriguez <timothy.rodriguez136@gmail.com>
2021-04-15 02:34:03 +0800

    feat(todo): add functionality to add, delete, and set to done

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,51 @@
 <script>
-	export let name;
+  import ToDo from './ToDo.svelte';
+
+  let name = '';
+  $: toDos = [];
+
+  function addToDo() {
+    if (name) {
+      toDos = [...toDos, name];
+      name = '';
+    }
+  }
+
+  function deleteToDo(event) {
+    const toDoToBeDeleted = event.detail.name;
+    toDos = toDos.filter((toDo) => toDo !== toDoToBeDeleted);
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>To-do List</h1>
+  <form on:click|preventDefault={addToDo}>
+    <input type="text" placeholder="Add To-Do" bind:value={name} />
+    <button>Add</button>
+  </form>
+  {#each toDos as toDo}
+    <ToDo name={toDo} on:deleteToDo={deleteToDo} />
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/ToDo.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let name = '';
+  $: isDone = false;
+
+  function deleteToDo() {
+    dispatch('deleteToDo', {
+      name,
+    });
+  }
+</script>
+
+<div class="toDoItem">
+  <input class="center" type="checkbox" bind:checked={isDone} />
+  <p class:strikeThrough={isDone}>{name}</p>
+  <button class="center" on:click={deleteToDo}>Delete</button>
+</div>
+
+<style>
+  .toDoItem {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .strikeThrough {
+    text-decoration: line-through;
+  }
+
+  .center {
+    align-self: center;
+  }
+
+  p {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+
+  @media (min-width: 640px) {
+    .toDoItem {
+      max-width: none;
+    }
+  }
+</style>
